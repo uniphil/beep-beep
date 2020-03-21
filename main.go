1e8fdc9fe84ef01144a0697f0562f4a0b01c0165
phil <uniphil@gmail.com>
2020-03-21 14:18:55 -0400

    kinda bad signup

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
 	_ "github.com/mattn/go-sqlite3"
-	_ "golang.org/x/crypto/bcrypt"
+	"golang.org/x/crypto/bcrypt"
 	"html/template"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -15,12 +17,17 @@ var (
 	// key must be 16, 24 or 32 bytes long (AES-128, AES-192 or AES-256)
 	key   = []byte("super-secret-key")
 	store = sessions.NewCookieStore(key)
+	cookie_name = "beep-beep"
 )
 
 var t *template.Template
 
+func normalizeEmail(e string) string {
+	return strings.ToLower(e)
+}
+
 func secret(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "cookie-name")
+	session, _ := store.Get(r, cookie_name)
 
 	// Check if user is authenticated
 	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
@@ -32,19 +39,96 @@ func secret(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "The cake is a lie!")
 }
 
+type LoginDetails struct {
+	Email    string
+	Password string
+}
+
+type LoginContext struct {
+	Action string
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	// session, _ := store.Get(r, cookie_name)
+
+	if r.Method == http.MethodPost {
+		db, err := sql.Open("sqlite3", "./accounts.db")
+		checkErr(err)
+
+		details := LoginDetails{
+			Email:    r.FormValue("email"),
+			Password: r.FormValue("password"),
+		}
+
+		email := normalizeEmail(details.Email)
+
+		q, err := db.Prepare("SELECT 1 FROM users WHERE email = ?")
+		checkErr(err)
+		rows, err := q.Query(email)
+
+		if rows.Next() {
+			panic("aiee already exists")
+		} else {
+			fmt.Println("kk cool")
+		}
+
+		rows.Close() //good habit to close
+
+		password, err := bcrypt.GenerateFromPassword([]byte("abc123"), bcrypt.DefaultCost)
+		if err != nil {
+			panic(err)
+		}
+
+
+		// insert
+		stmt, err := db.Prepare("INSERT INTO users(email, password) values(?,?)")
+		checkErr(err)
+
+		res, err := stmt.Exec(email, password)
+		checkErr(err)
+
+		id, err := res.LastInsertId()
+		checkErr(err)
+
+		fmt.Println(id)
+		t.ExecuteTemplate(w, "signup.tmpl", LoginContext{Action: "/signup"})
+	} else {
+		t.ExecuteTemplate(w, "signup.tmpl", LoginContext{Action: "/signup"})
+	}
+
+	// // Set user as authenticated
+	// session.Values["authenticated"] = true
+	// session.Save(r, w)
+}
+
 func login(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "cookie-name")
+	// session, _ := store.Get(r, cookie_name)
 
-	// Authentication goes here
-	// ...
+	if r.Method == http.MethodPost {
 
-	// Set user as authenticated
-	session.Values["authenticated"] = true
-	session.Save(r, w)
+		details := LoginDetails{
+			Email:    r.FormValue("email"),
+			Password: r.FormValue("password"),
+		}
+
+		password := details.Password
+		hash := "asdfasdf"
+		matches := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)) == nil
+		if !matches {
+			panic("oh nooooo")
+		}
+		t.ExecuteTemplate(w, "signup.tmpl", LoginContext{Action: "/login"})
+	} else {
+		t.ExecuteTemplate(w, "signup.tmpl", LoginContext{Action: "/login"})
+	}
+
+	// // Set user as authenticated
+	// session.Values["authenticated"] = true
+	// session.Save(r, w)
 }
 
 func logout(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "cookie-name")
+	session, _ := store.Get(r, cookie_name)
 
 	// Revoke users authentication
 	session.Values["authenticated"] = false
@@ -133,13 +217,23 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	http.HandleFunc("/", hello)
-	http.HandleFunc("/secret", secret)
-	http.HandleFunc("/login", login)
-	http.HandleFunc("/logout", logout)
-	http.ListenAndServe(":8080", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", hello)
+	r.HandleFunc("/secret", secret)
+	r.HandleFunc("/signup", signup)
+	r.HandleFunc("/login", login)
+	r.HandleFunc("/logout", logout)
+	http.ListenAndServe(":8080", r)
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	t.ExecuteTemplate(w, "signup.tmpl", "asdf")
+	if r.Method == http.MethodPost {
+		h, err := bcrypt.GenerateFromPassword([]byte("abc123"), bcrypt.DefaultCost)
+		if err != nil {
+			panic(err)
+		}
+		t.ExecuteTemplate(w, "signup.tmpl", h)
+	} else {
+		t.ExecuteTemplate(w, "signup.tmpl", "asdf")
+	}
 }
