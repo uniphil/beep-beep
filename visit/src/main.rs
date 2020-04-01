38511a24f0a8a592f41559e8e4bf614411705e11
phil <uniphil@gmail.com>
2020-04-01 14:52:31 -0400

    get visitor info

--- visit/src/main.rs
+++ visit/src/main.rs
@@ -1,10 +1,8 @@
-// use hyper::server::conn::Parts;
-use hyper::Uri;
+use std::collections::hash_map::DefaultHasher;
 use std::convert::Infallible;
+use std::hash::{Hash, Hasher};
 use std::net::SocketAddr;
-
-use hyper::{Body, Method, Request, Response, Server, StatusCode};
-use hyper::http;
+use hyper::{Body, Method, Request, Response, Server, StatusCode, Uri, http};
 use hyper::service::{make_service_fn, service_fn};
 
 static HELLO_PIXEL: &[u8] = &[
@@ -21,37 +19,49 @@ static BAD_PIXEL: &[u8] = &[
     0x01, 0x00, 0x00, 0x02, 0x02, 0x44, 0x01, 0x00, 0x3b,
 ];
 
-fn visitor(req: Request<Body>) -> Option<(Option<[u8; 8]>, String, String)> {
+fn visitor(req: &Request<Body>) -> Option<(Option<u64>, String, String)> {
     let headers = req.headers();
     let dnt = headers.get("dnt").map_or(false, |v| v == "1");
     let identifier = if false && dnt { None } else {
-        let ip = headers.get("x-forwarded-for");
-        let ua = headers.get("user-agent");
-        println!("{:?} {:?}", ip, ua);
-        Some(*b"xxxxxxxx")
+        let ip = headers.get("x-forwarded-for")
+            .and_then(|v| v.to_str().ok())
+            .unwrap_or_else(|| {
+                eprintln!("missing x-forwarded-for");
+                "127.0.0.1"
+            });
+        let ua = match headers.get("user-agent").and_then(|v| v.to_str().ok()) {
+            Some(v) => v,
+            None => { return None },  // prob not a browser visit
+        };
+        let mut hasher = DefaultHasher::new();
+        Hash::hash_slice(&[ip, ua], &mut hasher);
+        Some(hasher.finish())
     };
-    let referer = headers.get("referer");
-    if let Some(raw) = referer {
-        let uri = raw.to_str().unwrap().parse::<Uri>();
-        println!("uri {:?}", uri);
-        println!("host {:?}", (&uri).clone().as_ref().unwrap().host());
-        println!("path {:?}", uri.unwrap().path());
-    }
-    println!("ref {:?}", referer);
-    Some((identifier, "host".to_string(), "path".to_string()))
+    let (host, path) = match headers.get("referer")
+            .and_then(|v| v.to_str().ok())
+            .and_then(|v| v.parse::<Uri>().ok()) {
+        Some(uri) =>
+            if let Some(host) = uri.host() {
+                (host.to_string(), uri.path().to_string())
+            } else { return None },  // we need to know the host!
+        None => { return None },  // no referrer: can't know what to do with this req
+    };
+    Some((identifier, host, path))
 }
 
 async fn handle(req: Request<Body>) -> http::Result<Response<Body>> {
     if req.method() == &Method::GET {
         let p = req.uri().path();
         if p.is_ascii() && p.starts_with("/") && p.ends_with(".gif") && p.len() == (1 + 16 + 4) {
-            let _key = &p[1..17];
-            let _v = visitor(req);
-            return Response::builder()
-                .header("Content-Type", "image/gif")
-                .header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
-                .header("Pragma", "no-cache")
-                .body(Body::from(HELLO_PIXEL))            
+            let key = &p[1..17];
+            if let Some((identifier, host, path)) = visitor(&req) {
+                println!("{:?} {:?} {:?} {:?}", key, host, path, identifier);
+                return Response::builder()
+                    .header("Content-Type", "image/gif")
+                    .header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+                    .header("Pragma", "no-cache")
+                    .body(Body::from(HELLO_PIXEL))
+            }
         }
     }
     Response::builder()
